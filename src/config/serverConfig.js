f32d1fd74dddf3459fa86d3e2f4a5115ef0a8182
vanshdhawan60 <116313452+vanshdhawan60@users.noreply.github.com>
2025-01-01 00:53:01 +0530

    added jwt creation logic

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -5,4 +5,5 @@ dotenv.config();
 module.exports = {
     PORT: process.env.PORT,
     SALT_ROUNDS: parseInt(process.env.SALT_ROUNDS),
+    JSON_SECRET_KEY: process.env.JSON_SECRET_KEY
 }
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,4 +1,6 @@
 const UserRepository = require('../repository/user-repository');
+const { JSON_SECRET_KEY } = require('../config/serverConfig');
+const jwt = require('jsonwebtoken');
 
 class UserService {
 
@@ -22,6 +24,26 @@ class UserService {
             return true;
         } catch (error) {
             console.log("Something went wrong in service layer");
+            throw error;
+        }
+    }
+
+    generateToken (user) {
+        try {
+            const token = jwt.sign(user, JSON_SECRET_KEY, {expiresIn: '24h'});
+            return token;
+        } catch (error) {
+            console.log("Unable to generate user token!");
+            throw error;
+        }
+    }
+
+    verifyToken (token) {
+        try {
+            const user = jwt.verify(token, JSON_SECRET_KEY);
+            return user;
+        } catch (error) {
+            console.log("Unable to verify user token!");
             throw error;
         }
     }
