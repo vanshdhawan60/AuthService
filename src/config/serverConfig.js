9c29e79ed518988e34f3623cfd727c3954ab0d06
vanshdhawan60 <116313452+vanshdhawan60@users.noreply.github.com>
2025-01-02 02:39:28 +0530

    Added roles and user_roles tables + association many to many

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -5,5 +5,6 @@ dotenv.config();
 module.exports = {
     PORT: process.env.PORT,
     SALT_ROUNDS: parseInt(process.env.SALT_ROUNDS),
-    JSON_SECRET_KEY: process.env.JSON_SECRET_KEY
+    JSON_SECRET_KEY: process.env.JSON_SECRET_KEY,
+    DB_SYNC: process.env.DB_SYNC === 'true',
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const {PORT} = require('./config/serverConfig');
+const {PORT, DB_SYNC} = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
+const db = require('./models/index');
 
 const app = express();
 app.use(express.json());
@@ -11,6 +12,7 @@ const prepareAndStart = () => {
     app.use('/api', apiRoutes);
 
     app.listen(PORT, () => {
+        if (DB_SYNC) db.sequelize.sync({alter: true});
         console.log("Server started at port:", PORT);
     })
 }
